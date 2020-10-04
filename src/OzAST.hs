29a1b5c87b9452a2194a675febc9c0202d9f231d
Stewart Webb <sjwebb@student.unimelb.edu.au>
2020-10-04 16:42:46 +1100

    Implement types and formatters for all Oz instructions

---
+++ src/OzAST.hs
@@ -0,0 +1,109 @@
+-- |
+-- Module: OzAST
+-- Description: Data types which encode the abstract syntax tree of a valid Oz program.
+-- Maintainer: Stewart Webb <sjwebb@student.unimelb.edu.au>
+--             Ben Frengley <bfrengley@student.unimelb.edu.au>
+--
+-- An Oz program consists of a sequence of instructions, followed by an
+-- optional list of labelled blocks of other instructions representing named
+-- chunks of code that any other code can 'jump' to.
+
+module OzAST where
+
+import Control.Monad
+import Language.Haskell.TH
+
+-- Argument types
+
+data Framesize
+  = Framesize Word
+data StackSlot
+  = StackSlot Word
+data Register
+  = Register Word
+data IntegerConst
+  = IntegerConst Integer
+data StringConst
+  = StringConst String
+data Program = Program [Instruction] [LabelledBlock]
+  -- deriving (Eq, Show)
+
+data Label = Label String
+data LabelledBlock = LabelledBlock Label [Instruction]
+
+data Instruction
+  = InstrPushStackFrame Framesize
+  | InstrPopStackFrame Framesize
+  |
+    -- | Store into the memory location at stack slot S the value in register R
+    -- C equivalent:
+    -- > stackVar = regVar
+    InstrStore StackSlot Register
+  |
+    -- | Load into register R the value in stack slot S's memory location 
+    -- C equivalent:
+    -- > regVar = stackVar
+    InstrLoad Register StackSlot
+  |
+    -- | Load into register R the address of the memory location of stack slot S
+    -- C equivalent:
+    -- > regVar = &stackVar
+    InstrLoadAddress Register StackSlot
+  |
+    -- | Load into register R1 the value of the memory cell located in the
+    -- memory cell number corresponding to the numeric value stored in
+    -- register R2
+    -- C equivalent:
+    -- > regVar1 = *regVar2
+    InstrLoadIndirect Register Register
+  |
+    -- | Store into the memory cell located in the memory cell number corresponding
+    -- to the numeric value stored in register R1 the value of register R2
+    -- C equivalent:
+    -- > *regVar1 = regVar2
+    InstrStoreIndirect Register Register
+  | InstrIntConst Register IntegerConst
+  -- InstrRealConst
+  | InstrStringConst Register StringConst
+  | InstrAddInt Register Register Register
+  -- InstrAddReal
+  | InstrAddOffset Register Register Register
+  | InstrSubInt Register Register Register
+  -- InstrSubReal
+  | InstrSubOffset Register Register Register
+  | InstrMulInt Register Register Register
+  -- InstrMulReal
+  | InstrDivInt Register Register Register
+  -- InstrDivReal
+  | InstrNegInt Register Register
+  -- InstrNegReal
+  | InstrCmpEqualInt Register Register Register
+  | InstrCmpNotEqualInt Register Register Register
+  | InstrCmpGreaterThanInt Register Register Register
+  | InstrCmpGreaterEqualInt Register Register Register
+  | InstrCmpLessThanInt Register Register Register
+  | InstrCmpLessEqualInt Register Register Register
+  | InstrAnd Register Register Register
+  | InstrOr Register Register Register
+  | InstrNot Register Register
+  -- InstrIntToReal
+  | InstrMove Register Register
+  | InstrBranchOnTrue Register Label
+  | InstrBranchOnFalse Register Label
+  | InstrBranchUnconditional Label
+  | InstrCall Label
+  | InstrCallBuiltin BuiltinFunction
+  | InstrReturn
+  | InstrHalt
+  | InstrDebugRegister Register
+  | InstrDebugSlot StackSlot
+  | InstrDebugStack
+
+data BuiltinFunction
+  = BuiltinReadInt
+  -- BuiltinReadReal
+  | BuiltinReadBool
+  | BuiltinPrintInt
+  -- BuiltinPrintReal
+  | BuiltinPrintBool
+  | BuiltinPrintString
---
+++ src/OzWriter.hs
@@ -0,0 +1,64 @@
+module OzWriter where
+
+import Data.List (intercalate)
+
+import OzAST
+
+renderInstruction :: Instruction -> String
+
+renderInstruction (InstrPushStackFrame (Framesize fs)) = renderInstrWithArgs "push_stack_frame" [(show fs)]
+renderInstruction (InstrPopStackFrame (Framesize fs)) = renderInstrWithArgs "pop_stack_frame" [(show fs)]
+renderInstruction (InstrStore (StackSlot ss) r) = renderInstrWithArgs "store" [show ss, renderRegister r]
+renderInstruction (InstrLoad r (StackSlot ss)) = renderInstrWithArgs "load" [renderRegister r, show ss]
+renderInstruction (InstrLoadAddress r (StackSlot ss)) = renderInstrWithArgs "load_address" [renderRegister r, show ss]
+renderInstruction (InstrLoadIndirect rDest rSrc) = renderInstrWithArgs  "load_indirect" (renderRegister `map` [rDest, rSrc])
+renderInstruction (InstrStoreIndirect rDest rSrc) = renderInstrWithArgs "store_indirect" (renderRegister `map` [rDest, rSrc])
+renderInstruction (InstrIntConst rDest (IntegerConst int)) = renderInstrWithArgs "int_const" [renderRegister rDest, show int]
+renderInstruction (InstrStringConst rDest (StringConst str)) = renderInstrWithArgs "string_const" [renderRegister rDest, show str]
+renderInstruction (InstrAddInt rDest rLeft rRight) = renderInstrWithArgs "add_int" (renderRegister `map` [rDest, rLeft, rRight])
+renderInstruction (InstrAddOffset rDest rLeft rRight) = renderInstrWithArgs "add_offset" (renderRegister `map` [rDest, rLeft, rRight])
+renderInstruction (InstrSubInt rDest rLeft rRight) = renderInstrWithArgs "sub_int" (renderRegister `map` [rDest, rLeft, rRight])
+renderInstruction (InstrSubOffset rDest rLeft rRight) = renderInstrWithArgs "sub_offset" (renderRegister `map` [rDest, rLeft, rRight])
+renderInstruction (InstrMulInt rDest rLeft rRight) = renderInstrWithArgs "mul_int" (renderRegister `map` [rDest, rLeft, rRight])
+renderInstruction (InstrDivInt rDest rLeft rRight) = renderInstrWithArgs "div_int" (renderRegister `map` [rDest, rLeft, rRight])
+renderInstruction (InstrNegInt rDest rSrc) = renderInstrWithArgs "neg_int" (renderRegister `map` [rDest, rSrc])
+renderInstruction (InstrCmpEqualInt rDest rLeft rRight) = renderInstrWithArgs "cmp_eq_int" (renderRegister `map` [rDest, rLeft, rRight])
+renderInstruction (InstrCmpNotEqualInt rDest rLeft rRight) = renderInstrWithArgs "cmp_ne_int" (renderRegister `map` [rDest, rLeft, rRight])
+renderInstruction (InstrCmpGreaterThanInt rDest rLeft rRight) = renderInstrWithArgs "cmp_gt_int" (renderRegister `map` [rDest, rLeft, rRight])
+renderInstruction (InstrCmpGreaterEqualInt rDest rLeft rRight) = renderInstrWithArgs "cmp_ge_int" (renderRegister `map` [rDest, rLeft, rRight])
+renderInstruction (InstrCmpLessThanInt rDest rLeft rRight) = renderInstrWithArgs "cmp_lt_int" (renderRegister `map` [rDest, rLeft, rRight])
+renderInstruction (InstrCmpLessEqualInt rDest rLeft rRight) = renderInstrWithArgs "cmp_le_int" (renderRegister `map` [rDest, rLeft, rRight])
+renderInstruction (InstrAnd rDest rLeft rRight) = renderInstrWithArgs "and" (renderRegister `map` [rDest, rLeft, rRight])
+renderInstruction (InstrOr rDest rLeft rRight) = renderInstrWithArgs "or" (renderRegister `map` [rDest, rLeft, rRight])
+renderInstruction (InstrNot rDest rSrc) = renderInstrWithArgs "not" (renderRegister `map` [rDest, rSrc])
+renderInstruction (InstrMove rDest rSrc) = renderInstrWithArgs "move" (renderRegister `map` [rDest, rSrc])
+renderInstruction (InstrBranchOnTrue r (Label l)) = renderInstrWithArgs "branch_on_true" [renderRegister r, l]
+renderInstruction (InstrBranchOnFalse r (Label l)) = renderInstrWithArgs "branch_on_false" [renderRegister r, l]
+renderInstruction (InstrBranchUnconditional (Label l)) = renderInstrWithArgs "branch_uncond" [l]
+renderInstruction (InstrCall (Label l)) = renderInstrWithArgs "call" [l]
+renderInstruction (InstrCallBuiltin bif) = renderInstrWithArgs "call_builtin" [renderBuiltinFunction bif]
+renderInstruction (InstrReturn) = "return"
+renderInstruction (InstrHalt) = "halt"
+renderInstruction (InstrDebugRegister r) = renderInstrWithArgs "debug_reg" [(renderRegister r)]
+renderInstruction (InstrDebugSlot (StackSlot ssn)) = renderInstrWithArgs "debug_slot" [show ssn]
+renderInstruction (InstrDebugStack) = "debug_stack"
+
+renderInstrWithArgs :: String -> [String] -> String
+renderInstrWithArgs instr [arg] = unwords [instr, arg]
+renderInstrWithArgs instr args = unwords [instr, intercalate ", " args]
+
+renderLabelName :: Label -> String
+renderLabelName (Label labelName) = labelName
+
+renderRegister :: Register -> String
+renderRegister (Register rn) = "r" ++ show rn
+
+renderStackSlot :: StackSlot -> String
+renderStackSlot (StackSlot ss) = show ss
+
+renderBuiltinFunction :: BuiltinFunction -> String
+renderBuiltinFunction BuiltinReadInt = "read_int"
+renderBuiltinFunction BuiltinReadBool = "read_real"
+renderBuiltinFunction BuiltinPrintInt = "print_int"
+renderBuiltinFunction BuiltinPrintBool = "print_bool"
+renderBuiltinFunction BuiltinPrintString = "print_string"
