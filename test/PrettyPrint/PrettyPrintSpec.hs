24f81c9d14f81e0fef21a25becf01ac137d5e49c
Ben Frengley <ben.frengley@gmail.com>
2020-10-06 20:39:30 +1300

    Fix pretty print tests

--- test/PrettyPrint/PrettyPrintSpec.hs
+++ test/PrettyPrint/PrettyPrintSpec.hs
@@ -1,22 +1,19 @@
 -- Stewart Webb - sjwebb@student.unimelb.edu.au
 -- Ben Frengley - bfrengley@student.unimelb.edu.au
-
 -- this is needed to access __FILE__ macro
 {-# LANGUAGE CPP #-}
+
 module PrettyPrint.PrettyPrintSpec (spec) where
 
--- import System.Environment
-import System.Path
--- import Sy    stem.FilePath
-import Data.Text
-
-import Test.Hspec
-
 import AST
+import Data.Text
 import PrettyPrint
+import System.Path
+import Test.Hspec
+import Text.Parsec.Pos (initialPos)
 
 getFixturePath :: String -> String
-getFixturePath fileName = toUnrootedFilePath ((takeDirectory (fragment __FILE__)) </> fragment fileName)
+getFixturePath fileName = toUnrootedFilePath (takeDirectory (fragment __FILE__) </> fragment fileName)
 
 getFixture :: String -> IO String
 getFixture fixtureName = readFile (getFixturePath fixtureName)
@@ -27,28 +24,23 @@ spec = do
     it "pretty-prints simpleTest1" $ do
       prettyPrintTestCode <- getFixture "simpleTest1.roo"
       prettyPrint simpleText1Ast `shouldBe` Data.Text.pack prettyPrintTestCode
-        where
-            -- prettyPrintTestCode = "blah blah blah blah"
-            simpleText1Ast =
-                Program
-                    []
-                    []
-                    [
-                        Procedure
-                            (
-                                ProcHead
-                                    (Ident "main")
-                                    [
-                                        ProcParam (ParamBuiltinT TBool PassByRef) (Ident "arg1")
-                                    ]
-                            )
-                            (
-                                ProcBody
-                                    [
-                                        VarDecl (VarBuiltinT TBool) [Ident "myBool"]
-                                    ]
-                                    [
-                                        SAtom (Write (ConstStr "Hello World!"))
-                                    ]
-                            )
-                    ]
+  where
+    -- prettyPrintTestCode = "blah blah blah blah"
+    simpleText1Ast =
+      Program
+        []
+        []
+        [ Procedure
+            ( ProcHead
+                (initialPos "")
+                (Ident (initialPos "") "main")
+                [ ProcParam (initialPos "") (ParamBuiltinT TBool PassByRef) (Ident (initialPos "") "arg1")
+                ]
+            )
+            ( ProcBody
+                [ VarDecl (initialPos "") (VarBuiltinT TBool) [Ident (initialPos "") "myBool"]
+                ]
+                [ SAtom (initialPos "") (Write (ConstStr (initialPos "") "Hello World!"))
+                ]
+            )
+        ]
